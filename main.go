e66e72b000f02717a73bc9b10ae3e140d5081930
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-06 00:16:02 +0530

    feat: function

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	var card string = newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
