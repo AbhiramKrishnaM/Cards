912902c43a013befb62e0c541628ae0260ddca4b
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-07 19:08:07 +0530

    feat: added deal

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newdeck()
 
-	cards.print()
+	// cards.print()
 	// fmt.Println(cards)
+
+	fmt.Println("Play hand")
+
+	hand, remainingDeck := deal(cards, 2)
+	fmt.Println(hand, " balance ", remainingDeck)
 
 }
